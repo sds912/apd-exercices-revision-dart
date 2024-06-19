a801b407bb56a45962c7a7e57da4c08442387264
fatimata ba <bafatima@g.com>
2024-06-19 18:20:28 +0000

    mon premier commit

---
+++ index.dart
@@ -0,0 +1,28 @@
+
+void main() {
+  String nom = "Fatima Ba";
+  print(nom);
+  var age = 19;
+  print(age);
+  double pi = 3.14;
+  bool isFlutterAwesome = true;
+  final String nomComplet = "fatima ba";
+  const int vitesseLumiere = 29979458;
+  List<int> nombres = [1, 2, 3, 4, 5];
+  nombres.add(6);
+  print(nombres);
+  print(vitesseLumiere);
+  Map<String, dynamic> etudiant = {
+    "nom": "Amina",
+    "age": 20,
+    "class": "mathematiques",
+  Etudiant["note"] = 85;
+  void bonjour(String nom){
+    print("bonjour,$nom!");
+  }
+  };
+  int addition(int nombre1,int nombre2){
+    return nombre1 + nombre2;
+  }
+ 
+}
