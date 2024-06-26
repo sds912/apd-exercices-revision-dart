37b28ab15c6e91484093b14c5955e8cda11ebae8
awa2002 <awasamb937@gmail.com>
2024-06-26 21:40:29 +0000

    Mon premier projet

---
+++ exo.dart
@@ -0,0 +1,82 @@
+//import 'dart:io';
+//Variable de Dart 
+//Declaration de varible Nom et age
+void main(){
+  //String nom = "Samb";
+  //int age = 25;
+  //print(nom);
+  //print(age);
+
+// Types de varible
+  //double pi = 3.14;
+  //print(pi);
+  //bool isFlutterAwesome = true;
+  //print(isFlutterAwesome);
+//Variables finale et constantes
+  //final String nomComplet = 'AwaSamb'; 
+  //print(nomComplet);
+  //const int vitesseLumiere = 299792458; 
+  //print(vitesseLumiere);
+//LIST
+  //var nombres =["1","2","3","4","5"];
+  //print(nombres);
+// ajouter 6
+  //nombres.add("6");
+  //print(nombres);
+//MAPS
+  ///print(Etudiant);
+  //ajouter note a Etudiant
+  //Etudiant['note'] = 85;
+  //print(Etudiant);
+// FONCTIONS
+    //void bonjour(String nom) {
+      //print('Bonjour, $nom!');
+ // }
+  //bonjour('Papa'); 
+  //addition
+  //int addition(int a , int b){
+    //return a+b;
+  //} 
+  //int resultat = addition(5, 7);
+  //print(resultat);
+// les conditions if
+  //void verifierNombre(int nombre) {
+  //if (nombre > 0) {
+    //print('Le nombre est positif.');
+  //} else if (nombre < 0) {
+    //print('Le nombre est négatif.');
+  //} else {
+    //print('Le nombre est zéro.');
+  //}
+   
+//}
+//verifierNombre(0);
+// boucle for
+ //for (int i = 1; i <= 10; i++) {
+    //print(i);
+  //}
+//boucle while
+ //int i = 10;
+ //while (i >= 1) {
+    //print(i);
+    //i--;
+  //}
+//division
+  //double division(int a, int b) {
+  //if (b == 0) {
+    //throw Exception('Le dénominateur ne peut pas être zéro');
+  //}
+  //return a / b;
+//}
+//try {
+    //double resultat = division(10, 2);
+    //print(resultat);
+  //} catch (e) {
+    //print(e);
+  //}
+  //classe
+  
+}
+
+
+
